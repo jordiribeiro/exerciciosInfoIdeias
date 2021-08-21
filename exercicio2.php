153c9e004213f7267b898267c21f972e568f2f9e
Jordi Ribeiro <jordiribeirofonseca@gmail.com>
2021-08-21 18:19:27 -0300

    #Exercicio2

--- exercicio2.php
+++ exercicio2.php
@@ -1,7 +1,30 @@
 <?php
+$array[];
+
+function PrimoInferior($n){
+	if($n < 0){
+		return 0;
+	}
+	else{
+		for($x=0;$x<$n;$++){	
+			$c=0;
+			for($j=0;$j<$n;$j++){
+				if($x%($j+1)==0){
+					c++;
+				}
+			}
+			if($c==2){
+				$array=array_push($x);
+			}
+		}
+		echo $array;
+	}
+}
 
 
+echo PrimoInferior(10);
+#echo PrimoInferior(30);
 
-
-
+		
+		
 ?>
